7a9794727f93625cfe50455c324a56eab2289ac7
Reinaldo Mendoza <rmendoza83@gmail.com>
2021-04-06 03:34:33 -0500

    Adding toast functionality to show an appropiate message after try to save the property

--- RoofstockChallenge.Web/ClientApp/src/app/app.module.ts
+++ RoofstockChallenge.Web/ClientApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,8 +9,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { DataService } from './services/data.service';
+import { MessageService } from 'primeng/api';
 
 import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -18,15 +21,18 @@ import { TableModule } from 'primeng/table';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' }
     ]),
     TableModule,
+    ToastModule
   ],
   providers: [
-    DataService
+    DataService,
+    MessageService
   ],
   bootstrap: [
     AppComponent
--- RoofstockChallenge.Web/ClientApp/src/app/components/home/home.component.ts
+++ RoofstockChallenge.Web/ClientApp/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PropertyModel } from 'src/app/models/property.model';
+import { MessageService } from 'primeng/api';
+
+import { PropertyModel } from '../../models/property.model';
 import { DataService } from '../../services/data.service';
 
 @Component({
@@ -10,7 +12,8 @@ export class HomeComponent implements OnInit {
   properties: PropertyModel[] = [];
   loading: boolean = false;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+    private messageService: MessageService) { }
 
   ngOnInit() {
     this.dataService.getList()
@@ -27,11 +30,27 @@ export class HomeComponent implements OnInit {
     this.dataService.postProperty(row)
       .subscribe(response => {
         if (response.Success) {
-
+          this.sendSuccessMessage();
         } else {
-
+          this.sendErrorMessage(response.ErrorMessage);
         }
         this.loading = false;
       });
+  }
+
+  private sendSuccessMessage() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'The Property has been saved into the database'
+    });
+  }
+
+  private sendErrorMessage(errorMessage: string) {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: errorMessage
+    });
   }
 }
